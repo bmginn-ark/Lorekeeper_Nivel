f129f46eb144f1d0bf3cc8c3d9a352b23a543aff
letddo <hswhguswjd@gmail.com>
2025-12-24 13:33:39 +0900

    login/register 번역

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
 @section('title')
-    Register
+    회원가입
 @endsection
 
 @section('content')
     @if ($userCount)
         <div class="row">
             <div class="col-md-6 offset-md-4">
-                <h1>Register</h1>
+                <h1>회원가입</h1>
             </div>
         </div>
 
         @if ($altRegistrations)
-            <h3 class="text-center">Alternate Registrations</h3>
+            <h3 class="text-center">SNS 회원가입</h3>
             @foreach ($altRegistrations as $provider => $site)
                 @if (isset($site['login']) && $site['login'])
                     <div class="text-center w-75 m-auto pt-2 pb-2">
@@ -23,13 +23,13 @@
             @endforeach
         @endif
 
-        <h3 class="mt-5 text-center">Regular Registration</h3>
+        <h3 class="mt-5 text-center">일반 회원가입</h3>
         <form method="POST" action="{{ route('register') }}">
             @csrf
             @honeypot
 
             <div class="form-group row">
-                <label for="name" class="col-md-4 col-form-label text-md-right">Username</label>
+                <label for="name" class="col-md-4 col-form-label text-md-right">닉네임</label>
 
                 <div class="col-md-6">
                     <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
@@ -43,7 +43,7 @@
             </div>
 
             <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">E-mail Address</label>
+                <label for="email" class="col-md-4 col-form-label text-md-right">이메일 주소</label>
 
                 <div class="col-md-6">
                     <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
@@ -57,7 +57,7 @@
             </div>
 
             <div class="form-group row">
-                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('비밀번호') }}</label>
 
                 <div class="col-md-6">
                     <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
@@ -71,7 +71,7 @@
             </div>
 
             <div class="form-group row">
-                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('비밀번호 확인') }}</label>
 
                 <div class="col-md-6">
                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
@@ -80,7 +80,7 @@
 
             @if (!Settings::get('is_registration_open'))
                 <div class="form-group row">
-                    <label for="name" class="col-md-4 col-form-label text-md-right">Invitation Key {!! add_help('Registration is currently closed. An invitation key is required to create an account.') !!}</label>
+                    <label for="name" class="col-md-4 col-form-label text-md-right">초대 키 {!! add_help('Registration is currently closed. An invitation key is required to create an account.') !!}</label>
 
                     <div class="col-md-6">
                         <input id="code" type="text" class="form-control{{ $errors->has('code') ? ' is-invalid' : '' }}" name="code" value="{{ old('code') }}" required autofocus>
@@ -95,7 +95,7 @@
             @endif
 
             <div class="form-group row">
-                {{ Form::label('dob', 'Date of Birth', ['class' => 'col-md-4 col-form-label text-md-right']) }}
+                {{ Form::label('dob', '생년월일', ['class' => 'col-md-4 col-form-label text-md-right']) }}
                 <div class="col-md-6">
                     {!! Form::date('dob', null, ['class' => 'form-control']) !!}
                 </div>
@@ -111,7 +111,7 @@
                     <div class="form-check">
                         <label class="form-check-label">
                             {!! Form::checkbox('agreement', 1, false, ['class' => 'form-check-input']) !!}
-                            I have read and agree to the <a href="{{ url('info/terms') }}">Terms of Service</a> and <a href="{{ url('info/privacy') }}">Privacy Policy</a>.
+                            나는 <a href="{{ url('info/terms') }}">이용약관</a>과 <a href="{{ url('info/privacy') }}">개인정보처리방침</a>을 읽었으며 이에 동의합니다.
                         </label>
                     </div>
                 </div>
